619c1b07e69fa56c81b1ca69d899dc80ed65776f
Daniel Roe <daniel@roe.dev>
2023-06-09 11:18:26 +0100

    chore: init

---
+++ components/Clock.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const now = new Date()
+
+const hour = useState(() => now.getHours())
+const minute = useState(() => now.getMinutes())
+
+if (process.client) {
+  setInterval(() => {
+    const now = new Date()
+    hour.value = now.getHours()
+    minute.value = now.getMinutes()
+  }, 1000)
+}
+</script>
+
+<template>
+  <div class="font-sans font-bold text-white text-[15rem]">
+    {{ hour }}:{{ minute }}
+  </div>
+</template>
