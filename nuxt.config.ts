e1445fd52a893173758b2a05d9a5e397e2916d53
Daniel Roe <daniel@roe.dev>
2023-06-15 10:29:28 +0100

    fix: move back to static site

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,5 +7,10 @@ export default defineNuxtConfig({
   },
   routeRules: {
     '/api/**': { isr: 60 * 60, swr: 60 * 60 },
+  },
+  nitro: {
+    prerender: {
+      routes: ['/api/repos']
+    }
   }
 })
